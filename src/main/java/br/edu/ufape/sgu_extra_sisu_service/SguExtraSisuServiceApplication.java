066988a7eb9ac9fd34f4ce345725b6446aca26b1
Thiago2704 <thiagoaraujo909090@gmail.com>
2025-12-03 15:39:40 -0300

    Adicionando dependencias

---
+++ src/main/java/br/edu/ufape/sgu_extra_sisu_service/SguExtraSisuServiceApplication.java
@@ -0,0 +1,13 @@
+package br.edu.ufape.sgu_extra_sisu_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SguExtraSisuServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SguExtraSisuServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/edu/ufape/sgu_extra_sisu_service/SguExtraSisuServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.ufape.sgu_extra_sisu_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SguExtraSisuServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
